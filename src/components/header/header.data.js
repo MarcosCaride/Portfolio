cb8864085e73a5372092f69ba6a617e4b0aa8440
Marcos <marcoscaride72@gmail.com>
2021-12-28 01:42:09 -0300

    Update Progress

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Skills',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonial',
+  },
+];
